7cdc5772b2b2129e6bb6ffa3fd387066a8673042
MauricioSilv <mausilva828@gmail.com>
2019-05-02 14:31:28 -0300

    Configuring Site Screens

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -23,7 +23,7 @@
 <body id="page-top">
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
                 <div class="container">
-                  <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                  <a class="navbar-brand js-scroll-trigger" href="#page-top">Family bets</a>
                   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -31,20 +31,33 @@
                   <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ml-auto">
                       <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+                      <a class="nav-link js-scroll-trigger" href="{{route('principal')}}">Bolão</a>
                       </li>
+                      @guest
+                        <li class="nav-item">
+                          <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Cadastro</a>
+                        </li>
+                       
+                      
+                      @else
                       <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                          <a class="nav-link">{{ Auth::user()->name }} <span class="caret"></span></a>
                       </li>
+
                       <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                          <a class="nav-link" href="#" onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();"> {{ __('Logout') }}</a>
                       </li>
-                      <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#team">Team</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                      </li>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                      
+                          
+                      @endguest
                     </ul>
                   </div>
                 </div>
@@ -53,6 +66,44 @@
         <main>
             @yield('content')
         </main>
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <span class="copyright">Copyright &copy; Your Website 2018</span>
+              </div>
+              <div class="col-md-4">
+                <ul class="list-inline social-buttons">
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fab fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">
+                      <i class="fab fa-linkedin-in"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <ul class="list-inline quicklinks">
+                  <li class="list-inline-item">
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                  <li class="list-inline-item">
+                    <a href="#">Terms of Use</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
     </div>
 
      <!-- Bootstrap core JavaScript -->
